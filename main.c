2447b1d845b0ff35b4f640a343d7284e371c7f00
Pedro Vitório Arouche Campos <srn.kierkegaard@protonmail.com>
2026-09-18 11:14:43 -0300

    add teste na main

--- main.c
+++ main.c
@@ -2,25 +2,38 @@
 #include <stdio.h>
 
 int main() {
-    Cofo cofo;
-    cofoCriar(&cofo);
-    if (cofoInserir(&cofo, 20)) {
-        printf("valor inserido com sucesso!");
-    }
-    if (cofoInserir(&cofo, 2)) {
-        printf("valor inserido com sucesso!");
-    }
-    if (cofoInserir(&cofo, 8)) {
-        printf("valor inserido com sucesso!");
-    }
-    if (cofoConsultar(&cofo, 8)) {
-        printf("o valor está na lista!");
-    }
-    if (cofoRemover(&cofo, 2)) {
-        printf("Valor removido com sucesso");
+    Cofo cofo = {0};
+    cofoCreate(&cofo, 3);
+
+    if (cofoInsert(&cofo, 20)) {
+        printf("Valor adicionado ao cofo!\n");
+    } else {
+        printf("Não foi possível adionar o valor ao cofo!\n");
     }
 
-    if (!cofoConsultar( &cofo, 2)) {
-        printf("não encontrei!");
+    if (cofoInsert(&cofo, 2)) {
+        printf("Valor adicionado ao cofo!\n");
+    } else {
+        printf("Não foi possível adionar o valor ao cofo!\n");
     }
+
+    if (cofoInsert(&cofo, 8)) {
+        printf("Valor adicionado ao cofo!\n");
+    } else {
+        printf("Não foi possível adionar o valor ao cofo!\n");
+    }
+
+    if (cofoConsult(&cofo, 8)) {
+        printf("Valor encontrado!\n");
+    } else {
+        printf("Valor não encontrado!\n");
+    }
+
+    if (cofoRemove(&cofo, 2)) {
+        printf("Valor removido com sucesso!\n");
+    } else {
+        printf("Não foi possível remover o valor!\n");
+    }
+
+    return 0;
 }
